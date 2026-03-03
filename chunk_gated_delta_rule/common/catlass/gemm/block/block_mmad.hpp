4a6073c1e0b9f7607477f43723402657d316e0ad
Wei_NaChuan <weinachuan1@huawei.com>
2026-03-03 19:45:16 +0800

    fix fwd gdn

--- chunk_gated_delta_rule/common/catlass/gemm/block/block_mmad.hpp
+++ chunk_gated_delta_rule/common/catlass/gemm/block/block_mmad.hpp
@@ -111,4 +111,5 @@ struct BlockMmadAiv {
 #include "catlass/gemm/block/block_mmad_dynamic_aiv.hpp"
 #include "catlass/gemm/block/block_mmad_streamk.hpp"
 #include "catlass/gemm/block/block_mmad_w4a4_per_token_per_channel_dequant.hpp"
+#include "catlass/gemm/block/block_mmad_pingpong_gdn.hpp"
 #endif // CATLASS_GEMM_BLOCK_BLOCK_MMAD_HPP
